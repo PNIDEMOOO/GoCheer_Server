479c64cef1af7756d40a014a2c89c85b267fa779
orient <646189444@qq.com>
2016-12-15 15:27:07 +0800

    the function of registar is done!

--- src/webAPP/SendMail.java
+++ src/webAPP/SendMail.java
@@ -19,7 +19,7 @@ public class SendMail {
                 client.resource("https://api.mailgun.net/v3/gocheer.donggu.me/messages");
         MultivaluedMapImpl formData = new MultivaluedMapImpl();
         formData.add("from", "Mailgun Sandbox <postmaster@gocheer.donggu.me>");
-        formData.add("to", "hongyu <452937660@qq.com>");
+        formData.add("to", "donggu <452937660@qq.com>");
         formData.add("subject", "账户激活邮件");
         formData.add("text", "Congratulations dongg, you just sent an email with Mailgun!  You are truly awesome!  You can see a record of this email in your logs: https://mailgun.com/cp/log");
         return webResource.type(MediaType.APPLICATION_FORM_URLENCODED).
