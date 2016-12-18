a06844d62220d04cebfae5e39b9a0963400dfd86
orient <646189444@qq.com>
2016-12-18 17:06:46 +0800

    the function to send a mail is done!

--- src/webAPP/SendMail.java
+++ src/webAPP/SendMail.java
@@ -17,7 +17,7 @@ import javax.ws.rs.core.MediaType;
  */
 public class SendMail {
 
-    public static  ClientResponse SendCongratulations(User user, Achievement achievement){
+    public static  void SendCongratulations(User user, Achievement achievement){
         String alas=user.getAlias();
         String email=user.getEmail();
         String text_title="Dear "+alas+"\n.";
