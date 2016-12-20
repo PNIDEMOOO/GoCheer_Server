cb17941312f3d349e36a6820347325a302ec426a
Winely <sherlockheroine@gmail.com>
2016-12-20 18:36:01 +0800

    add Update user info servlet

---
+++ src/webAPP/UpdateInfoServlet.java
@@ -0,0 +1,49 @@
+package webAPP;
+
+import dao.UserDAO;
+import entity.User;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by Donggu on 2016/12/20.
+ */
+@WebServlet(name = "UpdateInfoServlet")
+public class UpdateInfoServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String username = (String)request.getSession().getAttribute("user");
+        if(username==null){
+            response.sendRedirect("../login");
+            return;
+        }
+        request.setCharacterEncoding("UTF-8");
+        String password = request.getParameter("password");
+        String email = request.getParameter("email");
+        String gender = request.getParameter("gender");
+        String alias = request.getParameter("alias");
+        User user = UserDAO.getInstance().findById(username);
+        if(password!=null && !password.isEmpty()){
+            user.setPassword(password);
+        }
+        if(email!=null && !email.isEmpty()){
+            user.setEmail(email);
+        }
+        if(alias!=null&&!alias.isEmpty()){
+            user.setAlias(alias);
+        }
+        if(gender!=null){
+            user.setGender(Boolean.parseBoolean(gender));
+        }
+        UserDAO.getInstance().update(user);
+        response.sendRedirect(request.getRequestURL().toString());
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
